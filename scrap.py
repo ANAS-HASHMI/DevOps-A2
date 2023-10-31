866ebe7a4cdea7a9d7efdf9d5b0c1767bdffc253
Anas Hashmi <msdsf23mo25@pucit.edu.pk>
2023-10-31 12:04:57 -0400

    I am committing this by the file

---
+++ scrap.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_webpage(url):
+    try:
+        # Fetch the webpage content
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # Parse the HTML content
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Extract text from the HTML
+        text = soup.get_text()
+
+        # Print clean text
+        print(text)
+
+    except requests.RequestException as e:
+        print(f"Failed to retrieve the webpage: {e}")
+
+# Replace 'URL_HERE' with the URL of the webpage you want to scrape
+webpage_url = 'https://www.waheediqbal.info/courses/devops23'
+scrape_webpage(webpage_url)
